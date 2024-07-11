41bc38104706580dcd3452b46f8404347baab73f
shwelu2024 <118778546+shwelu2024@users.noreply.github.com>
2024-07-11 20:42:04 +0700

    Add files via upload

---
+++ vpsroot.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Mod By NevermoreSSH
+#echo "$crot    ALL=(ALL:ALL) ALL" >> /etc/sudoers;
+wget -qO- -O /etc/ssh/sshd_config https://raw.githubusercontent.com/NevermoreSSH/hop/main/sshd_config;
+systemctl restart sshd;
+clear;
+echo -e "ใส่รหัสผ่าน:";
+read -e pwe;
+usermod -p `perl -e "print crypt("$pwe","Q4")"` root;
+clear;
+printf "กรุณาบันทึกข้อมูลบัญชี VPS นี้
+============================================
+Akun Root (Akun Utama)
+Ip address = $(curl -Ls http://ipinfo.io/ip)
+Username   = root
+Password   = $pwe
+============================================
+echo "";
+exit;
